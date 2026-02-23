4a3b5805758e8d7f0b33b1a641e302bbda819e2d
anatole.b <anatole.boisserie@gmail.com>
2026-02-23 14:25:30 +0100

    Stats gloables, trophées quand supprimé

--- backend/routes.py
+++ backend/routes.py
@@ -962,8 +962,7 @@ async def get_global_stats(db: Session = Depends(get_db)):
             print(f"Erreur stats missions: {e}")
 
         try:
-            stats["total_trophies"] = db.query(models.UserTrophy).join(models.User).filter(
-                models.User.is_deleted == False,
+            stats["total_trophies"] = db.query(models.UserTrophy).filter(
                 models.UserTrophy.is_obtained == True
             ).count()
         except Exception as e:
